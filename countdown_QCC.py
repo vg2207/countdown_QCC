4a7977041e3532b78d38c3b5040ef697e03aaab5
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2024-11-05 16:50:33 +0700

    Update countdown_QCC.py

--- countdown_QCC.py
+++ countdown_QCC.py
@@ -411,7 +411,17 @@ elif selected_main_menu == "Q&A" :
                     mins10, secs10 = divmod(t10, 60)
                     timer10 = '{:02d}:{:02d}'.format(mins10, secs10)
                     st.markdown(f'<p class="big-font">{timer10}</p>', unsafe_allow_html=True)
-                    st.audio([1],
+                    sample_rate = 44100  # 44100 samples per second
+                    seconds = 1  # Note duration of 2 seconds
+                    
+                    frequency_la = 440  # Our played note will be 440 Hz
+                    
+                    # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+                    t = np.linspace(0, seconds, seconds * sample_rate, False)
+                    
+                    # Generate a 440 Hz sine wave
+                    note_la = np.sin(frequency_la * t * 2 * np.pi)
+                    st.audio(note_la,
                              sample_rate=sample_rate,
                              autoplay=True)
                     # winsound.Beep(frequency, duration)
